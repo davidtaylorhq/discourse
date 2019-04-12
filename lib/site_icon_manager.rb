cab883de5ae20725de137e123539d68f010a36d6
David Taylor <david@taylorhq.com>
2019-04-12 14:48:12 +0100

    FEATURE: Automatically create correct size PWA manifest icon

---
+++ lib/site_icon_manager.rb
@@ -0,0 +1,24 @@
+module SiteIconManager
+  %i{
+    manifest_icon
+  }.each do |name|
+    define_singleton_method("#{name}_url") do
+      icon = self.public_send(name)
+      icon&.url
+    end
+
+    define_singleton_method("absolute_#{name}_url") do
+      url = self.public_send("#{name}_url")
+      full_cdn_url(url)
+    end
+  end
+
+  def self.fallback_icon
+    SiteSetting.large_icon || SiteSetting.logo_small
+  end
+
+  def self.manifest_icon
+    OptimizedImage.create_for(fallback_icon, 512, 512)
+  end
+
+end
