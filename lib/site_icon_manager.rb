6d763ae2f970d14aee60fe2ae37be717c18446db
David Taylor <david@taylorhq.com>
2019-04-12 16:18:10 +0100

    REFACTOR: Use icon_manager fallback system for opengraph_image

--- lib/site_icon_manager.rb
+++ lib/site_icon_manager.rb
@@ -3,6 +3,7 @@ module SiteIconManager
     manifest_icon
     favicon
     apple_touch_icon
+    opengraph_image
   }.each do |name|
     define_singleton_method("#{name}_url") do
       icon = self.public_send(name)
@@ -33,5 +34,10 @@ module SiteIconManager
     # Always resize, must be exactly 180x180
     icon = SiteSetting.apple_touch_icon || fallback_icon
     OptimizedImage.create_for(icon, 180, 180)
+  end
+
+  def self.opengraph_image
+    # No specific size requirement, supply the originals
+    (SiteSetting.opengraph_image || fallback_icon)&.url
   end
 end
